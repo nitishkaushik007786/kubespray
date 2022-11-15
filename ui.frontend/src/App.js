8334eb8e8422cd641f803ebf34759feef913fd56
maxsun8 <maxsunsny@gmail.com>
2022-11-14 22:17:40 -0500

    initial prototype

--- ui.frontend/src/App.js
+++ ui.frontend/src/App.js
@@ -1,11 +1,13 @@
 import { Page, withModel } from '@adobe/aem-react-editable-components';
 import React from 'react';
+import StoreInfo from "./components/StoreInfo/StoreInfo";
 
 // This component is the application entry point
 class App extends Page {
   render() {
     return (
       <div>
+        <StoreInfo/>
         {this.childComponents}
         {this.childPages}
       </div>
---
+++ ui.frontend/src/components/StoreInfo/StoreInfo.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./StoreInfo.css"
+const StoreInfo = (props) => {
+    return (
+        <div className="store-info-selector">
+            <div className="store-info-selector_info">
+                <span className="store-info-selector_info_tagline">
+                    You're shopping
+                </span>
+                <span>&#183;</span>
+                <span className="store-info-selector_info_closing-time">
+                    Open until 8PM
+                </span>
+            </div>
+            <div className="store-info-selector_selector">
+                <span className="store-info-selector_selector_store-name">
+                    Andersen Hardware & Supply
+                </span>
+                <svg width="8" height="9.5" viewBox="0 0 8 5" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7.06 0.0300007L4 3.08333L0.94 0.0300007L0 0.970001L4 4.97L8 0.970001L7.06 0.0300007Z" fill="#303133"/>
+                </svg>
+            </div>
+        </div>
+    )
+}
+
+export default StoreInfo;
